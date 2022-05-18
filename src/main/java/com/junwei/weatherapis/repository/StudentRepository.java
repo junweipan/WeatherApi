aa8b4d60fd2af577d68403ce689edf6d3727d433
junwei <panjunweide@gmail.com>
2022-05-18 12:10:59 +1000

    delete useless class

--- src/main/java/com/junwei/weatherapis/repository/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.junwei.weatherapis.repository;
-import com.junwei.weatherapis.model.Student;
-import org.springframework.data.repository.CrudRepository;
-public interface StudentRepository extends CrudRepository<Student, Integer>
-{
-}
